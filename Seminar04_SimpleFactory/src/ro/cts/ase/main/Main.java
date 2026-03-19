4b4aa588f1eb09b9b3f55d4779c5931a36178407
visan-cristian <vsn_crististef@yahoo.com>
2026-03-19 17:31:38 +0200

    Seminar 4

--- Seminar04_SimpleFactory/src/ro/cts/ase/main/Main.java
+++ Seminar04_SimpleFactory/src/ro/cts/ase/main/Main.java
@@ -7,10 +7,12 @@ import ro.cts.ase.factory.Tipuri;
 public class Main {
     public static void main(String[] args){
         Depou depou = new Depou();
-        MijlocTransport autobuz = depou.getMijlocTransport(Tipuri.AUTOBUZ, 6, "B 49 ABC");
-        MijlocTransport tramvai = depou.getMijlocTransport(Tipuri.TRAMVAI, 20, "B 49 ABC");
+        MijlocTransport autobuz = depou.getMijlocTransport(Tipuri.AUTOBUZ, 4, "B100STB");
+        MijlocTransport tramvai = depou.getMijlocTransport(Tipuri.TRAMVAI, 20, "B101STB");
+        MijlocTransport trolebuz = depou.getMijlocTransport(Tipuri.TROLEBUZ, 4, "B102STB");
 
         autobuz.afiseazaDescriere();
         tramvai.afiseazaDescriere();
+        trolebuz.afiseazaDescriere();
     }
 }
